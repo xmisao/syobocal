0ea50087281fce14bf544534fc2acf3c2af95c0a
xmisao <mail@xmisao.com>
2014-04-13 09:47:24 +0900

    support TitleViewCount, TitleRankHistory, TitlePointHistory, TitlePointTop

--- bin/syobocal.rb
+++ bin/syobocal.rb
@@ -34,4 +34,48 @@ when "DB::ChGroupLookup"
   puts "Code: " + result.code.to_s
   puts "Message: " + (result.message || "")
   pp result
+when "DB::TitleViewCount"
+  puts Syobocal::DB::TitleViewCount.url(params)
+  result = Syobocal::DB::TitleViewCount.get(params)
+  puts "Code: " + result.code.to_s
+  puts "Message: " + (result.message || "")
+  if result.code == 200
+    puts "Title: " + result.title
+    puts "Type: " + result.type
+    puts "Columns: " + result.columns.join(', ')
+  end
+  pp result
+when "DB::TitleRankHistory"
+  puts Syobocal::DB::TitleRankHistory.url(params)
+  result = Syobocal::DB::TitleRankHistory.get(params)
+  puts "Code: " + result.code.to_s
+  puts "Message: " + (result.message || "")
+  if result.code == 200
+    puts "Title: " + result.title
+    puts "Type: " + result.type
+    puts "Columns: " + result.columns.join(', ')
+  end
+  pp result
+when "DB::TitlePointHistory"
+  puts Syobocal::DB::TitlePointHistory.url(params)
+  result = Syobocal::DB::TitlePointHistory.get(params)
+  puts "Code: " + result.code.to_s
+  puts "Message: " + (result.message || "")
+  if result.code == 200
+    puts "Title: " + result.title
+    puts "Type: " + result.type
+    puts "Columns: " + result.columns.join(', ')
+  end
+  pp result
+when "DB::TitlePointTop"
+  puts Syobocal::DB::TitlePointTop.url(params)
+  result = Syobocal::DB::TitlePointTop.get(params)
+  puts "Code: " + result.code.to_s
+  puts "Message: " + (result.message || "")
+  if result.code == 200
+    puts "Title: " + result.title
+    puts "Type: " + result.type
+    puts "Columns: " + result.columns.join(', ')
+  end
+  pp result
 end
--- lib/syobocal/db.rb
+++ lib/syobocal/db.rb
@@ -12,7 +12,7 @@ module Syobocal
       def parse(xml)
         xml = REXML::Document.new(xml)
 
-        result = Result.new
+        result = LookupResult.new
 
         result.code = xml.elements['TitleLookupResponse/Result/Code'].text.to_i
         result.message = xml.elements['TitleLookupResponse/Result/Message'].text
@@ -68,7 +68,7 @@ module Syobocal
       def parse(xml)
         xml = REXML::Document.new(xml)
 
-        result = Result.new
+        result = LookupResult.new
 
         result.code = xml.elements['ProgLookupResponse/Result/Code'].text.to_i
         result.message = xml.elements['ProgLookupResponse/Result/Message'].text
@@ -120,7 +120,7 @@ module Syobocal
       def parse(xml)
         xml = REXML::Document.new(xml)
 
-        result = Result.new
+        result = LookupResult.new
 
         result.code = xml.elements['ChLookupResponse/Result/Code'].text.to_i
         result.message = xml.elements['ChLookupResponse/Result/Message'].text
@@ -166,7 +166,7 @@ module Syobocal
       def parse(xml)
         xml = REXML::Document.new(xml)
 
-        result = Result.new
+        result = LookupResult.new
 
         result.code = xml.elements['ChGroupLookupResponse/Result/Code'].text.to_i
         result.message = xml.elements['ChGroupLookupResponse/Result/Message'].text
@@ -196,6 +196,70 @@ module Syobocal
       end
     end
 
+    module TitleViewCount
+      def get(params = {})
+        parse(open(url(params)))
+      end
+
+      def url(params)
+        'http://cal.syoboi.jp/db.php?Command=TitleViewCount' + Syobocal::DB.format_params_amp(params)
+      end
+
+      def parse(xml)
+        Syobocal::DB.parse_table_data(xml)
+      end
+
+      module_function :get, :url, :parse
+    end
+
+    module TitleRankHistory
+      def get(params = {})
+        parse(open(url(params)))
+      end
+
+      def url(params)
+        'http://cal.syoboi.jp/db.php?Command=TitleRankHistory' + Syobocal::DB.format_params_amp(params)
+      end
+
+      def parse(xml)
+        Syobocal::DB.parse_table_data(xml)
+      end
+
+      module_function :get, :url, :parse
+    end
+
+    module TitlePointHistory
+      def get(params = {})
+        parse(open(url(params)))
+      end
+
+      def url(params)
+        'http://cal.syoboi.jp/db.php?Command=TitlePointHistory' + Syobocal::DB.format_params_amp(params)
+      end
+
+      def parse(xml)
+        Syobocal::DB.parse_table_data(xml)
+      end
+
+      module_function :get, :url, :parse
+    end
+
+    module TitlePointTop
+      def get(params = {})
+        parse(open(url(params)))
+      end
+
+      def url(params)
+        'http://cal.syoboi.jp/db.php?Command=TitlePointTop' + Syobocal::DB.format_params_amp(params)
+      end
+
+      def parse(xml)
+        Syobocal::DB.parse_table_data(xml)
+      end
+
+      module_function :get, :url, :parse
+    end
+
     def self.format_params_amp(params)
       return "" if params.length == 0
 
@@ -204,8 +268,50 @@ module Syobocal
       }.join('&')
     end
 
-    class Result < DelegateClass(Array)
+    def self.parse_table_data(xml)
+      xml = REXML::Document.new(xml)
+
+      result = TableResult.new
+
+      result.code = xml.elements['TableData/Result/Code'].text.to_i
+      result.message = xml.elements['TableData/Result/Message'].text
+
+      result.columns = []
+      if result.code == 200
+        result.title = xml.elements['TableData/Title'].text
+        result.type = xml.elements['TableData/Type'].text
+
+        xml.elements.each('TableData/Columns/Value'){|item|
+          result.columns << item.text
+        }
+
+        xml.elements.each('TableData/Line'){|line|
+          line_data = {}
+          line.elements.each_with_index{|value, index|
+            key = result.columns[index]
+            if key == "Date"
+              line_data[key] = Date.parse(value.text)
+            else
+              line_data[key] = value.text ? value.text.to_i : nil
+            end
+          }
+          result << line_data
+        }
+      end
+
+      result
+    end
+
+    class LookupResult < DelegateClass(Array)
       attr_accessor :code, :message
+
+      def initialize
+        super([])
+      end
+    end
+
+    class TableResult < DelegateClass(Array)
+      attr_accessor :code, :message, :title, :type, :columns
 
       def initialize
         super([])
